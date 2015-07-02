ef85119f6c582ee9b9a8b01b8318e987d7e2fdc5
DenisOrel <4uzhoy>
2015-07-02 09:09:30 +0300

    Add class

--- HostingWindowsForms/IVentsService.cs
+++ HostingWindowsForms/IVentsService.cs
@@ -1,14 +1,9 @@
 ﻿using System.ServiceModel;
 using ConecctorOneC;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Security.Cryptography.X509Certificates;
 
 namespace HostingWindowsForms
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IVentsService
     {
--- HostingWindowsForms/VentsService.cs
+++ HostingWindowsForms/VentsService.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
+﻿using System.Collections.Generic;
 using ConecctorOneC;
 
 namespace HostingWindowsForms
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class VentsService : IVentsService
     {
         List<Connection.ClassifierMeasure> IVentsService.GetList()
